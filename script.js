151c8b35b3ce187be8d071af13743fb013f317b1
deppfellow <raid.rafif3@gmail.com>
2024-03-10 20:32:13 +0700

    Add another logic to end. Some bug still there where when modal confirm the deleted sometime more than 1

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, pages, readStatus) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.readStatus = readStatus
-}
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+};
 
 function addBookToLibrary(title, author, pages, readStatus) {
     const book = new Book(title, author, pages, readStatus)
 
-    myLibrary.push(book)
+    myLibrary.push(book);
     showBookInLibrary();
-}
+
+};
 
 function showBookInLibrary() {
     const bookList = document.getElementById("table-body");
-    // bookList.textContent = '';
 
     for (let i = 0; i < myLibrary.length; i++) {
         const bookRow = document.createElement("tr");
@@ -50,36 +50,75 @@ function showBookInLibrary() {
 
         // Book's read btn
         const readBtn = document.createElement("button");
-        readBtn.setAttribute("class", "tb-action-btn read-status");
+        readBtn.setAttribute("id", "status-btn");
+        readBtn.setAttribute("class", "tb-action-btn");
         readBtn.textContent = "Status";
         bookAction.appendChild(readBtn);
 
         // Book's delete btn
         const deleteBtn = document.createElement("button");
+        deleteBtn.setAttribute("id", "del-btn");
         deleteBtn.setAttribute("class", "tb-action-btn");
         deleteBtn.textContent = "Delete";
         bookAction.appendChild(deleteBtn);
+    };
 
-    }
-}
+    myLibrary = [];
+};
 
 const bookForm = document.getElementById("book-form");
 bookForm.addEventListener("submit", function (event) {
-    event.preventDefault()
+    event.preventDefault();
     addBookToLibrary(
         event.target[0].value,
         event.target[1].value,
         +event.target[2].value,
         event.target[3].checked
-    )
-
-    console.log(myLibrary)
-
-    // const output = {
-    //     title: event.title.value,
-    //     author: event.bookData.author.value,
-    //     pages: +event.pages.value,
-    //     readStatus: event.readStatus.checked
-    // };
-
+    );
 });
+
+// const statusButton = document.getElementById("status-btn");
+// statusButton.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     // const getCurrentBookStatus = event.parentElement;
+//     // console.log(getCurrentBookStatus);
+
+//     console.log(event);
+// });
+
+function modalAction(tdBook) {
+    const modal = document.getElementById("ls-modal-wrapper");
+    modal.showModal()
+
+    modal.addEventListener("click", (event) => {
+        if (event.target.id === "modal-confirm-btn") {
+            tdBook.remove();
+            modal.close();
+        } else {
+            modal.close();
+        };
+    });
+}
+
+
+function listenClicks() {
+    document.addEventListener("click", (event) => {
+        const { target } = event;
+
+        if (target.id === "status-btn") {
+            let tdStatus = target.parentElement.previousElementSibling;
+            if (tdStatus.textContent === "true") {
+                tdStatus.textContent = "false";
+            } else if (tdStatus.textContent === "false") {
+                tdStatus.textContent = "true";
+            };
+
+        } else if (target.id === "del-btn") {
+            let tdBook = target.parentElement.parentElement;
+            modalAction(tdBook);
+
+        };
+    });
+};
+
+listenClicks();
